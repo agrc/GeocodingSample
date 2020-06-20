0d0b279eed56965cbe1dff60662b89e6c662a82b
Gary Ray <gary.ray@gmail.com>
2020-06-20 13:13:28 -0600

    Fix incorrect 'score' attribute to 'acceptScore'

--- VB/Program.vb
+++ VB/Program.vb
@@ -3,8 +3,9 @@
     Sub Main()
         'Register to receive an API key at developer.mapserv.utah.gov
         Dim g = new Geocoder("your api key here")
+        Dim acceptScoreThreshold As Integer = 90
         Dim location = g.Locate("123 South Main Street", "SLC", New Dictionary(Of String, Object) From {
-                                   {"score", 90},
+                                   {"acceptScore", acceptScoreThreshold},
                                    {"spatialReference", 4326}
                                    })
 
