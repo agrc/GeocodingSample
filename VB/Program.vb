288044b473aea60584e12b391892bcf402d92007
Gary Ray <gary.ray@gmail.com>
2020-06-21 10:44:55 -0600

    Replace spatialReference magic number with common constants

--- VB/Program.vb
+++ VB/Program.vb
@@ -1,6 +1,10 @@
 ﻿Module Program
 
     Sub Main()
+        'Common spatialReference wkid constants - see http://resources.arcgis.com/en/help/main/10.1/018z/pdf/geographic_coordinate_systems.pdf
+        Const UtmZone12N As Integer = 26912 'Default
+        Const LatLongWgs84 As Integer = 4326
+        Const WebMercator As Integer = 3857
         'Register to receive an API key at developer.mapserv.utah.gov
         Dim g = new Geocoder("your api key here")
         Dim acceptScoreThreshold As Integer = 90
